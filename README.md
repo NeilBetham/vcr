4a224523ce72a63428713fed55bf7c665188634e
Neil Betham <nbetham@me.com>
2021-01-18 18:59:37 -0500

    Add protos for storing to tape

--- README.md
+++ README.md
@@ -8,9 +8,17 @@ Tape Library File Archiver
 - Encrypt files on tape
 
 # Reasons for building this
-- Bacula is not suited to my use case
-- No other solutions really exist to archive to a tape drive
+- Bacula is not suited to my use case and has a lot of moving parts
+- Couldn't find another solution which is directly suited to my use case
 
 # Notes
 Currently I am targeting the IBM TS3310 with this tool as that is the only tape library
 I have access to.
+
+# Encryption
+In order to facilitate storing files securely at rest they will need to be encrypted.
+When VCR stores a file onto tape it will store a header that contains the file info
+alongside the file data in the cipher text. This will keep all relevant metadata secure
+unless accessed by someone with the proper keys. There will also be a clear text header
+before the cipher text to indicate the payload length and whether the payload is
+encrypted.
--- src/main.cpp
+++ src/main.cpp
@@ -26,39 +26,6 @@ int main(int argc, char** argv) {
   }
 
   if(changer_device) {
-    vcr::Changer mtx(args::get(changer_device));
-    spdlog::info("Media In Library:");
-    for(const auto& medium : mtx.list_media()) {
-      spdlog::info("Tape: {}", medium.id());
-    }
-
-    spdlog::info("Slots in Library:");
-    for(const auto& slot : mtx.list_slots()) {
-      spdlog::info("Slot [{}] - Full: {}, Medium ID: '{}'", slot.address(), slot.full(), slot.medium_id());
-    }
-
-    spdlog::info("Drives in Library:");
-    for(const auto& drive : mtx.list_drives()) {
-      spdlog::info("Drive [{}] - Full: {}, Medium ID: '{}', Source Addr: {}", drive.address(), drive.full(), drive.medium_id(), drive.source_address());
-
-    }
-
-    spdlog::info("Robots in Library:");
-    for(const auto& robot : mtx.list_robots()) {
-      spdlog::info("Robot [{}]", robot.address());
-
-    }
-
-    spdlog::info("Try moving media into and out of drive");
-    auto media = mtx.list_media();
-    auto drives = mtx.list_drives();
-    spdlog::info("Loading media {} into drive {}", media.front().id(), drives.front().address());
-    mtx.load_media(media.front(), drives.front());
-    spdlog::info("Media loaded");
-    sleep(30);
-    spdlog::info("Unloading media {} from drive {} to slot {}", media.front().id(), drives.front().address(), media.front().address());
-    mtx.unload_media(drives.front(), vcr::Slot(media.front().address(), false, ""));
-    spdlog::info("Media unloaded");
 
   }
 
